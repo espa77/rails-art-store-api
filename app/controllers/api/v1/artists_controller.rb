12617277b30297d78440cc44af0ac766b33f5573
espa77 <ethansteiner@gmail.com>
2016-02-23 13:29:09 -0500

    minor fixes to controllers

--- app/controllers/api/v1/artists_controller.rb
+++ app/controllers/api/v1/artists_controller.rb
@@ -1,36 +1,30 @@
 class Api::V1::ArtistsController < ApplicationController
 
-  # GET /artists
-  # GET /artists.json
   def index
     render json: Artist.all
   end
 
-  # GET /artists/1
-  # GET /artists/1.json
   def show
     render json: @artist
   end
 
   def create
-    artist = Artist.create(artist_params)
-    render json: artist
+    @artist = Artist.create(artist_params)
+    render json: @artist
   end
 
   def update
-    artist = Artist.find(params[:id])
-    artist.update(artist_params)
-    render json: artist
+    @artist = Artist.find(params[:id])
+    @artist.update(artist_params)
+    render json: @artist
   end
 
   def destroy
-    artist = Artist.find(params[:id])
-    artist.destroy
-    render nothing: true
+    @artist = Artist.find(params[:id])
+    render json: @artist.destroy
   end
 
   private
-
     def artist_params
       params.require(:artist).permit(:name, :location, :bio, :asset)
     end
--- app/controllers/api/v1/exhibitions_controller.rb
+++ app/controllers/api/v1/exhibitions_controller.rb
@@ -1,55 +1,32 @@
 class Api::V1::ExhibitionsController < ApplicationController
   before_action :set_exhibition, only: [:show, :update, :destroy]
 
-  # GET /exhibitions
-  # GET /exhibitions.json
   def index
     render json: Exhibition.all
   end
 
-  # GET /exhibitions/1
-  # GET /exhibitions/1.json
   def show
     render json: @exhibition
   end
 
-  # POST /exhibitions
-  # POST /exhibitions.json
   def create
     @exhibition = Exhibition.new(exhibition_params)
-
-    if @exhibition.save
-      render json: @exhibition, status: :created, location: @exhibition
-    else
-      render json: @exhibition.errors, status: :unprocessable_entity
-    end
+    render json: @exhibition
   end
 
-  # PATCH/PUT /exhibitions/1
-  # PATCH/PUT /exhibitions/1.json
   def update
     @exhibition = Exhibition.find(params[:id])
-
-    if @exhibition.update(exhibition_params)
-      head :no_content
-    else
-      render json: @exhibition.errors, status: :unprocessable_entity
-    end
+    @exhibition.update(exhibition_params)
+    render json: @exhibition
   end
 
-  # DELETE /exhibitions/1
-  # DELETE /exhibitions/1.json
   def destroy
+    @exhibition = Exhibition.find(params[:id])
     render json: @exhibition.destroy
   end
 
   private
-
-    def set_exhibition
-      @exhibition = Exhibition.find(params[:id])
-    end
-
     def exhibition_params
-      params.require(:exhibition).permit(:location, :title)
+      params.require(:exhibition).permit(:location, :title, :piece_ids)
     end
 end
