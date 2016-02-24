6bcb9a35145d65558fc83c82e1a40c1ff357b3ac
espa77 <ethansteiner@gmail.com>
2016-02-24 11:03:58 -0500

    images showing and uploading

--- app/serializers/artist_serializer.rb
+++ app/serializers/artist_serializer.rb
@@ -1,5 +1,5 @@
 class ArtistSerializer < ActiveModel::Serializer
-  attributes :id, :name, :location, :bio, :piece_ids, :exhibition_ids, :asset
+  attributes :id, :name, :location, :bio, :piece_ids, :exhibition_ids, :asset_url
   has_many :pieces, serializer: PieceNoAssociationSerializer
   # has_many :exhibitions, through: :pieces
 end
--- app/serializers/piece_serializer.rb
+++ app/serializers/piece_serializer.rb
@@ -1,6 +1,6 @@
 class PieceSerializer < ActiveModel::Serializer
   embed :ids, include: true
-  attributes :id, :title, :style, :subject, :medium, :size, :price, :artist_id, :asset
+  attributes :id, :title, :style, :subject, :medium, :size, :price, :artist_id, :asset_url
   has_one :artist, serializer: ArtistNoAssociationSerializer
   has_one :exhibition
 end
